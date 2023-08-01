199895e69f6f71efdca11370fe1330ce8bbb53b4
Yatin2108 <yatin@browserstack.com>
2023-08-01 14:47:03 +0530

    TO Integration

--- resources/conf/wdio-on-prem.conf.js
+++ resources/conf/wdio-on-prem.conf.js
@@ -11,7 +11,18 @@ const overrides = {
         acceptInsecureCerts: true
     }],
     baseUrl: 'https://bstackdemo.com/',
-    services: ['chromedriver']
+    services: ['chromedriver',
+        ['browserstack', {
+            testObservability: true,
+            testObservabilityOptions: {
+                user: process.env.BROWSERSTACK_USERNAME,
+                key: process.env.BROWSERSTACK_ACCESS_KEY,
+                projectName: 'browserstack-examples-cucumber-webdriverio',
+                buildName: 'browserstack-examples-cucumber-webdriverio build',
+                buildTag: 'WDIO Cucumber'
+            },
+        }]
+    ],
 }
 
 exports.config = _.defaultsDeep(overrides, defaults.config)
